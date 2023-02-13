ddd250ec365c15ea8a11fd37507206e3ed44b06e
Dan Luo <luo_dana@yahoo.com>
2023-02-14 02:25:28 +1000

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -6,10 +6,11 @@ def pin_to_ipfs(data):
 	#YOUR CODE HERE
 	with open("sample.json", "w+") as outfile:
 		json.dump(data, outfile) 
-	files = {'file': str(data)}	
-	print(files)
+	files = {'file': outfile}	
+	#print(files)
 	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', files=files, auth=('2LgqjZLmpHnRS52JcP9fDQEH83S','205aa3dbd378a8f1bc392ddd6f9cbf15'))
 	print(response.text)
+	print('============')
 	file_list = response.json()
 	cid = file_list['Pins'][0]
 	return cid
